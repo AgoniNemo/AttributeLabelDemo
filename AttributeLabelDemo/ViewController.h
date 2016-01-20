9fc47afa2c4dda4c932e459e452046a1a0cd027e
Nemo <XZC168520@163.com>
2016-01-20 16:16:53 +0800

    Initial Commit

---
+++ AttributeLabelDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AttributeLabelDemo
+//
+//  Created by GMCC on 16/1/20.
+//  Copyright © 2016年 N. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
